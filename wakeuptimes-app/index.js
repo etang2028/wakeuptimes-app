eaf20c1665bead46ed9985d11588171cfe5daa66
etang2028 <ellentang2024@gmail.com>
2026-01-22 12:37:33 -0500

    add dom manipulation

--- wakeuptimes-app/index.js
+++ wakeuptimes-app/index.js
@@ -1,32 +1,27 @@
 const calcBtn = document.getElementById("calc-btn");
+const wakeUpHoursDiv = document.getElementById("wakeup-hours-div");
+
 calcBtn.onclick = calcWakeUpTimes;
 
 function calcWakeUpTimes() {
-	const now = new Date();
-	console.log(now.toLocaleString("en-US", {
-		timeStyle: "short"
-	}));
-
-	const fallAsleepTime = new Date(now);
-	fallAsleepTime.setMinutes(
-		fallAsleepTime.getMinutes() + 14
-	);
-	console.log("You will fall asleep at", 
-		fallAsleepTime.toLocaleString("en-US", {
-		timeStyle: "short"
-	}));
+	
+	const fallAsleepTime = new Date();
+	fallAsleepTime.setMinutes(fallAsleepTime.getMinutes() + 14);
 
 	const wakeUpTime = new Date(fallAsleepTime);
-	const wakeUpTimes = [];
+	wakeUpHoursDiv.innerHTML = ""; // clear previous results
+
 	for (let i = 0; i < 6; i++) {
-		wakeUpTime.setMinutes(
-			wakeUpTime.getMinutes() + 90
-		);
-		const wakeUpTimeString = wakeUpTime.toLocaleString("en-US", {
+		wakeUpTime.setMinutes(wakeUpTime.getMinutes() + 90);
+		const timeString = wakeUpTime.toLocaleString("en-US", {
 			timeStyle: "short"
 		});
-		wakeUpTimes.push(wakeUpTimeString);
-	}
 
-	console.log("Wake-up times:", wakeUpTimes.join(', '));
+		// create div for each time
+		const cycleDiv = document.createElement("div");
+		cycleDiv.classList.add("cycle");
+		cycleDiv.setAttribute("id", `cycle-${i}`);
+		cycleDiv.textContent = timeString;
+		wakeUpHoursDiv.appendChild(cycleDiv);
+	}
 }
