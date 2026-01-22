75e68212dd5a01509fcbf0b5edd38bd6be0414f4
etang2028 <ellentang2024@gmail.com>
2026-01-22 12:13:16 -0500

    initial commit

---
+++ wakeuptimes-app/index.js
@@ -0,0 +1,26 @@
+const now = new Date();
+console.log(now.toLocaleString("en-US", {
+	timeStyle: "short"
+}));
+
+const fallAsleepTime = new Date(now);
+fallAsleepTime.setMinutes(
+	fallAsleepTime.getMinutes() + 14
+);
+console.log(fallAsleepTime.toLocaleString("en-US", {
+	timeStyle: "short"
+}));
+
+const wakeUpTime = new Date(fallAsleepTime);
+const wakeUpTimes = [];
+for (let i = 0; i < 6; i++) {
+	wakeUpTime.setMinutes(
+		wakeUpTime.getMinutes() + 90
+	);
+	const wakeUpTimeString = wakeUpTime.toLocaleString("en-US", {
+		timeStyle: "short"
+	});
+	wakeUpTimes.push(wakeUpTimeString);
+}
+
+console.log(wakeUpTimes.join(', '));
