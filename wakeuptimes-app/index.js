60e286fd174ee681fada475deb9caaa822e34781
etang2028 <ellentang2024@gmail.com>
2026-01-22 12:28:13 -0500

    added sleep cycle calculation with button click

--- wakeuptimes-app/index.js
+++ wakeuptimes-app/index.js
@@ -1,26 +1,32 @@
-const now = new Date();
-console.log(now.toLocaleString("en-US", {
-	timeStyle: "short"
-}));
+const calcBtn = document.getElementById("calc-btn");
+calcBtn.onclick = calcWakeUpTimes;
 
-const fallAsleepTime = new Date(now);
-fallAsleepTime.setMinutes(
-	fallAsleepTime.getMinutes() + 14
-);
-console.log(fallAsleepTime.toLocaleString("en-US", {
-	timeStyle: "short"
-}));
-
-const wakeUpTime = new Date(fallAsleepTime);
-const wakeUpTimes = [];
-for (let i = 0; i < 6; i++) {
-	wakeUpTime.setMinutes(
-		wakeUpTime.getMinutes() + 90
-	);
-	const wakeUpTimeString = wakeUpTime.toLocaleString("en-US", {
+function calcWakeUpTimes() {
+	const now = new Date();
+	console.log(now.toLocaleString("en-US", {
 		timeStyle: "short"
-	});
-	wakeUpTimes.push(wakeUpTimeString);
-}
+	}));
 
-console.log(wakeUpTimes.join(', '));
+	const fallAsleepTime = new Date(now);
+	fallAsleepTime.setMinutes(
+		fallAsleepTime.getMinutes() + 14
+	);
+	console.log("You will fall asleep at", 
+		fallAsleepTime.toLocaleString("en-US", {
+		timeStyle: "short"
+	}));
+
+	const wakeUpTime = new Date(fallAsleepTime);
+	const wakeUpTimes = [];
+	for (let i = 0; i < 6; i++) {
+		wakeUpTime.setMinutes(
+			wakeUpTime.getMinutes() + 90
+		);
+		const wakeUpTimeString = wakeUpTime.toLocaleString("en-US", {
+			timeStyle: "short"
+		});
+		wakeUpTimes.push(wakeUpTimeString);
+	}
+
+	console.log("Wake-up times:", wakeUpTimes.join(', '));
+}
